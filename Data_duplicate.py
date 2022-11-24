def2af9e20835207b5b20e00284606285ec2f92d
cansuhugul18 <cansu18.hugul@gmail.com>
2022-11-24 17:37:32 +0300

    Satırdaki kelimlern virgülle ayrılması

---
+++ Data_duplicate.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = pd.read_excel('C:/Users/Cansu/Desktop/veri.xlsx')
+
+print(data)
+
+Alinanlar= data['Alinanlar'].str.split(',').explode().value_counts()
+
+print(Alinanlar)
+
+
+
+
